951a8bfc56df4809d4bf9bebe422f4c44aa0c11e
yoshiki-shibata <yoshiki-shibata@yoshikiatanoAir.AirPort>
2023-05-30 22:43:44 +0900

    add:button

--- pages/tailwind/index.tsx
+++ pages/tailwind/index.tsx
@@ -1,11 +1,130 @@
 // pages/tailwind.tsx
 
+import {ComponentPropsWithRef, FC, forwardRef} from "react";
+import {cva, cx, VariantProps} from "class-variance-authority";
+import {Simplify} from "type-fest";
+import {twMerge} from "tailwind-merge";
+import clsx from "clsx";
+
 export default function TailwindPage() {
     return (
-
-    <div className="m-128 p-144 bg-brand-blue">
-        <p className="text-white">Hello, World!</p>
-    </div>
+        <div>
+            <Home/>
+        </div>
+    )
+        ;
+}
+export type BoxProps = VariantProps<typeof buttonStyles>
+const buttonStyles = cva(`w-full rounded-lg px-5 py-2.5 text-white`, {
+    variants: {
+        intent: {
+            primary: cx(
+                `bg-blue-500 hover:bg-blue-700 disabled:bg-blue-200`,
+                `active:border-blue-400 active:ring-blue-400`,
+                `outline-none focus:outline-none focus:ring-2`,
+                `focus:border-blue-600 focus:ring-blue-600`,
+                `focus-within:border-blue-600 focus-within:ring-blue-600`,
+                `focus-visible:border-blue-600 focus-visible:ring-blue-600`
+            ),
+            success: cx(
+                `bg-green-500 hover:bg-green-700 disabled:bg-green-200`,
+                `active:border-green-400 active:ring-green-400`,
+                `outline-none focus:outline-none focus:ring-2`,
+                `focus:border-green-600 focus:ring-green-600`,
+                `focus-within:border-green-600 focus-within:ring-green-600`,
+                `focus-visible:border-green-600 focus-visible:ring-green-600`
+            ),
+            danger: cx(
+                `bg-red-500 hover:bg-red-700 disabled:bg-red-200`,
+                `active:border-red-400 active:ring-red-400`,
+                `outline-none focus:outline-none focus:ring-2`,
+                `focus:border-red-600 focus:ring-red-600`,
+                `focus-within:border-red-600 focus-within:ring-red-600`,
+                `focus-visible:border-red-600 focus-visible:ring-red-600`
+            )
+        },
+        size: {
+            small: `text-sm`,
+            medium: `text-base`,
+            large: `text-lg`
+        },
+    },
+    defaultVariants: {
+        intent: `primary`,
+        size: `medium`
+    },
+})
+type Props = Simplify<Pick<
+    ComponentPropsWithRef<`button`>, 'tabIndex' | 'type' | 'className' | 'ref' | 'children' | 'disabled'> & BoxProps>
+export const Button: FC<Props> = forwardRef(
+    ({
+         children,
+         intent = 'primary',
+         size = 'medium',
+         type = 'button',
+         disabled = false,
+         className,
+         ...props
+     },
+     ref
+    ) => {
+        return (
+            <button
+                ref={ref}
+                type={type}
+                className={twMerge(
+                    buttonStyles({intent, size}),
+                    clsx(`${disabled ? `cursor-not-allowed` : `cursor-pointer`}`),
+                    className
+                )}
+                disabled={disabled}
+                {...props}
+            >
+                {children}
+            </button>
+        )
+    }
 )
-    ;
+Button.displayName = 'Button'
+export const Home = () => {
+    return (
+        <div className='mx-auto my-12 flex w-full max-w-[20rem] flex-col gap-4'>
+            <Button intent={'primary'} size={'large'}>
+                Large Primary Button
+            </Button>
+            <Button intent={'primary'} size={'large'} disabled>
+                Large Primary Button Disabled
+            </Button>
+            <Button intent={'primary'} size={'medium'}>
+                Medium Primary Button
+            </Button>
+            <Button intent={'primary'} size={'small'}>
+                Small Primary Button
+            </Button>
+            <Button intent={'danger'} size={'large'}>
+                Large Danger Button
+            </Button>
+            <Button intent={'danger'} size={'large'} disabled>
+                Large Danger Button Disabled
+            </Button>
+            <Button intent={'danger'} size={'medium'}>
+                Medium Danger Button
+            </Button>
+            <Button intent={'danger'} size={'small'}>
+                Small Danger Button
+            </Button>
+            <Button intent={'success'} size={'large'}>
+                Large Success Button
+            </Button>
+            <Button intent={'success'} size={'large'} disabled>
+                Large Success Button Disabled
+            </Button>
+            <Button intent={'success'} size={'medium'}>
+                Medium Success Button
+            </Button>
+            <Button intent={'success'} size={'small'}>
+                Small Success Button
+            </Button>
+        </div>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,64 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      kosugi: ['Kosugi', 'sans-serif'],
+      noto: ['Noto Sans JP', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+    },
     extend: {
-      colors: {
-        'brand-blue': '#1DA1F2',
+      // https://getcssscan.com/css-box-shadow-examples
+      boxShadow: {
+        bebop: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
       },
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
+      // https://uicolors.app/create
+      // https://tailwind.ink/code
+      // https://tailwindcss.com/docs/theme#colors
+      colors: {
+        blue: {
+          50: '#f3f8fc',
+          100: '#e5f0f9',
+          200: '#c5e0f2',
+          300: '#93c8e6',
+          400: '#59abd7',
+          500: '#3391c4',
+          600: '#277fb5',
+          700: '#1e5d86',
+          800: '#1d506f',
+          900: '#1d435d',
+          950: '#132b3e',
+        },
+        red: {
+          50: '#fdf3f3',
+          100: '#fbe5e5',
+          200: '#f9cfcf',
+          300: '#f4adad',
+          400: '#eb7e7e',
+          500: '#de5555',
+          600: '#bd3232',
+          700: '#aa2b2b',
+          800: '#8d2727',
+          900: '#752727',
+          950: '#3f1010',
+        },
+        green: {
+          50: '#effaf4',
+          100: '#d8f3e2',
+          200: '#b4e6c9',
+          300: '#82d3aa',
+          400: '#4fb886',
+          500: '#288d60',
+          600: '#1d7e55',
+          700: '#186446',
+          800: '#155038',
+          900: '#124230',
+          950: '#09251b',
+        },
       },
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
